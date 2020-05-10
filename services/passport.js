e8f8ac16ca4d39cc90903c181cb4cba2ae376898
Steven Zhu <stevenzhu03@gmail.com>
2020-05-10 18:04:30 -0400

    creating user model in mongodb

--- services/passport.js
+++ services/passport.js
@@ -1,3 +1,10 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("../config/keys");
+const mongoose = require("mongoose");
+
+const User = mongoose.model("users");
+
 passport.use(
   new GoogleStrategy(
     {
@@ -6,9 +13,9 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken", accessToken);
-      console.log("refreshToken", refreshToken);
-      console.log("profiles", profile);
+      new User({
+        googleID: profile.id,
+      }).save();
     }
   )
 );
