07660facff4d35757069558d36d61b45f1e9431c
Steven Zhu <stevenzhu03@gmail.com>
2020-05-10 16:32:28 -0400

    refactoring services and routes

--- index.js
+++ index.js
@@ -12,18 +12,22 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
     },
-    (accessToken) => {
-      console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+      console.log("accessToken", accessToken);
+      console.log("refreshToken", refreshToken);
+      console.log("profiles", profile);
     }
   )
 );
 
 app.get(
-  "auth/google",
+  "/auth/google",
   passport.authenticate("google", {
     scope: ["profile", "email"],
   })
 );
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
