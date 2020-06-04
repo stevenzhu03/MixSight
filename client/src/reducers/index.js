ecee1e5f1f5b8999ccad581926962645e721bf62
Steven Zhu <stevenzhu03@gmail.com>
2020-06-03 23:41:09 -0400

    adding survey reducer to combine reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
+import surveryReducer from "./surveyReducer";
 
 export default combineReducers({
   auth: authReducer,
+  survey: surveryReducer,
 });
