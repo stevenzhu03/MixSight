ba0186cf2737d5f5e2758972bf1dcd53a86ec353
Steven Zhu <stevenzhu03@gmail.com>
2020-06-16 13:20:04 -0400

    removing body parser

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
-// const bodyParser = require("body-parser");
 const passport = require("passport");
 const keys = require("./config/keys");
 
